e3ac1487d850018a0e1728fd685085a5f29bad23
Sahana2803 <sahupavi123@gmail.com>
2024-06-19 17:02:11 +0530

    Today's Workout

---
+++ dynamic.js
@@ -0,0 +1,54 @@
+let car={
+    carname:"honda",
+    carcolor:"black",
+    carprice:80000
+}
+for(let v in car)
+{
+    console.log(car[v]);  
+}
+var name1="kashi";
+var s=name1
+const y=86;
+
+const x=86;
+document.write(name1)
+let ab=10;
+let b=20;
+console.log(ab+b)
+document.write(ab+b)
+let a=10;
+if(a>=18)
+{
+    document.write("eligible to vote")
+}
+else{
+    document.write("Not eligible")
+}
+let sname="sahana is an polite and intelligent girl"
+console.log(sname.length)
+var newsname=sname.slice(5,11)
+console.log(newsname)
+var newsname=sname.substring(5,11)
+console.log(newsname)
+var newsname=sname.substr(5,sname.length)
+console.log(newsname)
+var arr=new Array("Sahana","Ranjani","Pavithran");
+console.log(arr[2])
+console.log(arr)
+var animals=new Array("dog","cat","rabbit","goat")
+console.log(animals)
+console.log(animals.sort())
+run()
+function run()
+{
+    var arr1=new Array("Sahana","Ranjani","Pavithran")
+    console.log(arr1)
+    document.write(arr1)
+}
+let myfunction=()=>{
+    var arr2=["rubika","surya Prabha","rubika","vikasini"]
+    console.log(arr2)
+    document.write(arr2)
+}
+myfunction()
